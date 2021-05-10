fe73fccb9c074cbbae11e1f69ad8d44be136ecc4
mcsheffey <dev.h1620@gmail.com>
2021-05-10 15:40:54 +0700

    ad project

--- script/index.js
+++ script/index.js
@@ -1,10 +1,18 @@
 let choosen = 0;
+let isAnimated = false;
+let documentheight = document.body.clientHeight;
 
 const homePage = document.querySelector("#home");
 const blogPage = document.querySelector("#blog");
 const prevBtn = document.querySelector("#left-btn");
 const nextBtn = document.querySelector("#right-btn");
 const allDots = document.querySelectorAll(".fa-circle");
+const projectComponents = document.querySelectorAll(".project-item");
+const svgText = document.querySelector(".svg-text");
+projectComponents[0].style.transform = "translateY(400px)";
+projectComponents[0].style.opacity = "0";
+projectComponents[1].style.transform = "translateY(400px)";
+projectComponents[1].style.opacity = "0";
 
 const allPages = [homePage, blogPage];
 
@@ -19,6 +27,8 @@ prevBtn.addEventListener("click", () => {
       allPages[choosen].classList.add("fade-in");
       allDots[choosen].style.color = "#d63a5f";
       allDots[1].style.color = "black";
+
+      svgText.classList.add("svg-text");
     }, 50);
   } else {
   }
@@ -35,11 +45,31 @@ nextBtn.addEventListener("click", () => {
       allPages[choosen].classList.add("fade-in");
       allDots[choosen].style.color = "#d63a5f";
       allDots[0].style.color = "black";
+      svgText.classList.remove("svg-text");
     }, 50);
   } else {
   }
 });
+let yAxis;
+window.addEventListener("scroll", () => {
+  yAxis = documentheight - window.pageYOffset;
+
+  if (yAxis < 600 && yAxis > 200) {
+    projectComponents[0].style.transition = "all ease 1.5s";
+    projectComponents[0].style.transform = "translateY(0px)";
+    projectComponents[0].style.opacity = "1";
+  } else if (yAxis < -250) {
+    projectComponents[1].style.transition = "all ease 1.5s";
+    projectComponents[1].style.transform = "translateY(0px)";
+    projectComponents[1].style.opacity = "1";
+  } else if (yAxis > 600) {
+    projectComponents[0].style.transform = "translateY(400px)";
+    projectComponents[0].style.opacity = "0";
+    projectComponents[1].style.transform = "translateY(400px)";
+    projectComponents[1].style.opacity = "0";
+  }
+});
 
 allPages[0].classList.add("fade-in");
-
+allPages[0].classList.remove("slide-to-left");
 allDots[0].style.color = "#d63a5f";
