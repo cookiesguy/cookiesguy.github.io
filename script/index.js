24fb48fd30de95ba377f45df05d8b7b8f78f3bd3
fstdev16 <dev.h1620@gmail.com>
2021-09-23 00:24:31 +0700

    update website

--- script/index.js
+++ script/index.js
@@ -4,8 +4,6 @@ let documentHeight = document.body.clientHeight;
 const projectComponents = document.querySelectorAll('.project-block');
 const svgText = document.querySelector('.svg-text');
 const navLink = document.querySelector('.list');
-const menuButton = document.querySelector('.menu-btn');
-const sideBar = document.querySelector('.side-bar');
 const container = document.querySelector('.container');
 projectComponents[0].style.transform = 'translateY(1000px)';
 projectComponents[0].style.opacity = '0';
@@ -15,7 +13,7 @@ navLink.style.transform = 'translateY(-80px)';
 let yAxis;
 window.addEventListener('scroll', () => {
    yAxis = documentHeight - window.pageYOffset;
-   if (yAxis < 600 && yAxis > 200) {
+   if (yAxis < 800 && yAxis > 200) {
       projectComponents[0].style.transition = 'all ease 1.5s';
       projectComponents[0].style.transform = 'translateY(0px)';
       projectComponents[0].style.opacity = '1';
@@ -30,17 +28,6 @@ window.setTimeout(() => {
    navLink.style.transition = 'all ease 0.8s';
    navLink.style.transform = 'translateY(0px)';
 }, 50);
-
-menuButton.addEventListener('click', () => {
-   console.log('fired');
-   sideBar.style.transform = 'translateX(0px)';
-   container.style.filter = 'brightness(50%)';
-});
-
-function closeSideBar() {
-   sideBar.style.transform = 'translateX(-140px)';
-   container.style.filter = 'brightness(100%)';
-}
 
 function showHand() {
    const hand = document.getElementById('hand-pointer');
--- script/sidebar.js
+++ script/sidebar.js
@@ -1,13 +1,14 @@
-const menuButton = document.querySelector(".menu-btn");
-const sideBar = document.querySelector(".side-bar");
-const container = document.querySelector(".container");
-menuButton.addEventListener("click", () => {
-  console.log("fired");
-  sideBar.style.transform = "translateX(0px)";
-  container.style.filter = "brightness(50%)";
+const menuButton = document.querySelector('.menu-btn');
+const sideBar = document.querySelector('.side-bar');
+
+const disableDiv = document.getElementById('disable');
+
+menuButton.addEventListener('click', () => {
+   sideBar.style.transform = 'translateX(0px)';
+   disableDiv.style.display = 'block';
 });
 
 function closeSideBar() {
-  sideBar.style.transform = "translateX(-140px)";
-  container.style.filter = "brightness(100%)";
+   sideBar.style.transform = 'translateX(-140px)';
+   disableDiv.style.display = 'none';
 }
